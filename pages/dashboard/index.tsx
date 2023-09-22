d9a563cc39dca59e191bcbd07782b9ce41d7dc0c
Haziq Khairi <ikhairulhaziq11@gmail.com>
2023-09-22 22:40:40 +0800

    style: add ability to customize Bootstrap

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,12 @@
+import "@/styles/styles.scss";
+
 export default function Dashboard() {
-	return (
-		<div>
-			<h1>Dashboard</h1>
-			<p>Here is the place where the graph and charts will be display</p>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>Dashboard</h1>
+      <div className="alert alert-success">
+        <p>Here is the place where the graph and charts will be display</p>
+      </div>
+    </div>
+  );
 }
