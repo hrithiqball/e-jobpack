65599c01b2bb323dedc27d74ede7ce2ade2596c5
Harith Iqbal <harithmu@gmail.com>
2023-09-22 21:07:13 +0800

    chore(structure): added file and new folder for better workflow

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+	return (
+		<div>
+			<h1>Dashboard</h1>
+			<p>Here is the place where the graph and charts will be display</p>
+		</div>
+	);
+}
---
+++ pages/sign-in/index.tsx
@@ -0,0 +1,9 @@
+export default function SignIn() {
+	return (
+		<div>
+			<h1>Sign In Page</h1>
+			<p>Text input here for signing in</p>
+			<p>can also sign in using google and microsoft</p>
+		</div>
+	);
+}
