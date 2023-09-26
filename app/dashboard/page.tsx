57cfa52c70d41b55552de94df4bfbc19c3774f40
Harith <harithmu@gmail.com>
2023-09-26 23:29:31 +0800

    build: upgraded nextjs version to 13.5.3 | feat(asset): fetched data from db for assets

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,47 @@
-import React from "react";
+"use client";
 
-interface User {
-  id: number;
-  name: string;
+import { asset } from "@prisma/client";
+import React, { useState } from "react";
+
+export default function Dashboard() {
+	const [asset, setAsset] = useState<asset[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const fetchData = async () => {
+		setIsLoading(true);
+		const result = await fetch("/api/asset", { method: "GET" });
+		// TODO use standardized response from /lib/result
+		const response = await result.json();
+
+		if (result.ok) {
+			setAsset(response.data);
+			// TODO use message as alert
+			console.log(response.message);
+			setIsLoading(false);
+		} else {
+			setIsLoading(false);
+			console.log(response.message);
+		}
+	};
+
+	return (
+		<div className="bg-emeraldGreenDark text-white">
+			<h1 className="text-3xl font-bold underline">Assets</h1>
+			<button
+				onClick={fetchData}
+				disabled={isLoading}
+				className={`px-4 py-2 font-semibold text-white transition-colors duration-200 transform rounded-md bg-emerald-500 hover:bg-emerald-600 focus:outline-none focus:bg-emerald-600 active:bg-emerald-700 ${
+					isLoading ? "cursor-not-allowed" : ""
+				}`}
+			>
+				{isLoading ? "Loading..." : "Fetch Assets"}
+			</button>
+
+			<ul>
+				{asset.map((asset) => (
+					<li key={asset.uid}>{asset.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
-const page = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await res.json();
-  return (
-    <div className="bg-emeraldGreenDark text-white">
-      <h1 className="text-3xl font-bold underline">Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default page;
