647e829f6943698604cb866d69bec013802bc758
Harith <harithmu@gmail.com>
2023-10-01 16:34:51 +0800

    feat(api): completed api for history and maintenance. * not tested yet due to rls

---
+++ app/api/history/[uid]/route.ts
@@ -0,0 +1,43 @@
+// GET one history
+
+import { prisma } from "@/lib/initPrisma";
+import { ResponseMessage } from "@/lib/result";
+import { history } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * This asynchronous function handles GET requests.
+ * @param {NextRequest} nextRequest - The incoming HTTP request.
+ * @param {string} uid - The unique identifier of the history.
+ *
+ * @returns {Promise<NextResponse>} Returns a promise that resolves with the result of the operation on the history.
+ */
+export async function GET(
+	nextRequest: NextRequest,
+	{ params }: { params: { uid: string } }
+): Promise<NextResponse> {
+	const uid = params.uid;
+	const history: history | null = await prisma.history.findUnique({
+		where: { uid },
+	});
+
+	if (history) {
+		return new NextResponse(
+			JSON.stringify(
+				ResponseMessage(200, `Successfully fetched history {${uid}}`, history)
+			),
+			{
+				status: 200,
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+	} else {
+		return new NextResponse(
+			JSON.stringify(ResponseMessage(404, `History {${uid}} not found`)),
+			{
+				status: 404,
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+	}
+}
