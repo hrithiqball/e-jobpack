381636d6f2010a39d6c7b4ea1129bbb306eb7581
Restyled.io <commits@restyled.io>
2023-12-01 23:15:28 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images : {
-    remotePatterns : [
+  images: {
+    remotePatterns: [
       {
-        protocol : "https",
-        hostname : "www.nu-heat.co.uk",
-        port : "",
-        pathname :
-            "/wp-content/uploads/2020/10/Underfloor-heating-manifold.jpg",
+        protocol: "https",
+        hostname: "www.nu-heat.co.uk",
+        port: "",
+        pathname: "/wp-content/uploads/2020/10/Underfloor-heating-manifold.jpg",
       },
     ],
   },
