33dbf11e1267f67a26fdb76c7aa6df7183f96925
Restyled.io <commits@restyled.io>
2023-12-01 23:15:24 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "www.nu-heat.co.uk",
-				port: "",
-				pathname: "/wp-content/uploads/2020/10/Underfloor-heating-manifold.jpg",
-			},
-		],
-	},
+  images : {
+    remotePatterns : [
+      {
+        protocol : "https",
+        hostname : "www.nu-heat.co.uk",
+        port : "",
+        pathname :
+            "/wp-content/uploads/2020/10/Underfloor-heating-manifold.jpg",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
