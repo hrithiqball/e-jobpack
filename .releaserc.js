5d4696ddba6c05067b3334caff5a74b008ffd39e
Harith <harithmu@gmail.com>
2023-12-07 22:03:14 +0800

    feat: added github actions for ci and converted commonjs to modulejs

---
+++ .releaserc.js
@@ -0,0 +1,8 @@
+export const branches = ['main'];
+export const plugins = [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator',
+  '@semantic-release/changelog',
+  '@semantic-release/github',
+  '@semantic-release/git',
+];
