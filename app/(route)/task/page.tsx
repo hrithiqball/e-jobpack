76312f8af091daf9a12a6a115a05bae293c3e997
Harith <harithmu@gmail.com>
2024-01-14 02:18:05 +0800

    style: updated layout for task(desktop)

--- app/(route)/task/page.tsx
+++ app/(route)/task/page.tsx
@@ -1,12 +1,12 @@
-import Task from '@/components/client/task/TaskComponent';
 import { fetchMaintenanceList } from '@/lib/actions/maintenance';
+import TaskComponent from '@/components/client/task/TaskComponent';
 
 export default async function TaskPage() {
   const maintenanceList = await fetchMaintenanceList();
 
   return (
-    <div className="flex flex-col h-full">
-      <Task maintenanceList={maintenanceList} />
+    <div className="flex flex-1 flex-col h-full">
+      <TaskComponent maintenanceList={maintenanceList} />
     </div>
   );
 }
